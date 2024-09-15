6ea64c0b1e90bef0cbb4e68a83d7d5207cffab69
Lucksey <98761938+Lucksey@users.noreply.github.com>
2024-09-15 14:52:23 +0300

    6 lesson: save Register debug

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -12,33 +12,34 @@ export class AuthService {
   constructor(
     @InjectModel(UserModel) private readonly UserModel: ModelType<UserModel>,
     private readonly jwtService: JwtService
-  ) {}
-
-  async login(dto: AuthDto) {
-    const user = await this.validateUser(dto)
-
-    const tokens = await this.issueTokenPair(String(user._id))
-
-    return {
-      user: this.returnUserFieslds(user),
-      ...tokens,
-    }
+  ) {
   }
 
-  async getNewTokens({refreshToken}: RefreshTokenDto) {
-   if(!refreshToken) throw new UnauthorizedException('Please sign in!')
+  async login(dto: AuthDto) {
+    const user = await this.validateUser(dto);
 
-    const result = await this.jwtService.verifyAsync(refreshToken)
-    if(!result) throw new UnauthorizedException('Invalid token or expired!')
-
-    const user = await this.UserModel.findById(result._id)
-
-    const tokens = await this.issueTokenPair(String(user._id))
+    const tokens = await this.issueTokenPair(String(user._id));
 
     return {
-      user: this.returnUserFieslds(user),
-      ...tokens,
-    }
+      user: this.returnUserFields(user),
+      ...tokens
+    };
+  }
+
+  async getNewTokens({ refreshToken }: RefreshTokenDto) {
+    if (!refreshToken) throw new UnauthorizedException("Please sign in!");
+
+    const result = await this.jwtService.verifyAsync(refreshToken);
+    if (!result) throw new UnauthorizedException("Invalid token or expired!");
+
+    const user = await this.UserModel.findById(result._id);
+
+    const tokens = await this.issueTokenPair(String(user._id));
+
+    return {
+      user: this.returnUserFields(user),
+      ...tokens
+    };
   }
 
   async register(dto: AuthDto) {
@@ -55,12 +56,12 @@ export class AuthService {
       password: await hash(dto.password, salt)
     });
 
-    const tokens = await this.issueTokenPair(String(newUser._id))
-
+    const tokens = await this.issueTokenPair(String(newUser._id));
+    newUser.save()
     return {
-      user: this.returnUserFieslds(newUser),
-      ...tokens,
-    }
+      user: this.returnUserFields(newUser),
+      ...tokens
+    };
   }
 
   async validateUser(dto: AuthDto): Promise<UserModel> { // получаем ДТО
@@ -73,24 +74,24 @@ export class AuthService {
     return user;
   }
 
-  async issueTokenPair(userId:string) {
-    const data = {_id: userId}
-    
-    const refreshToken = await this.jwtService.signAsync(data,{
-      expiresIn: '15d'
-    })
-    const accessToken = await this.jwtService.signAsync(data,{
-      expiresIn: '1h'
-    })
-    return {refreshToken, accessToken }
+  async issueTokenPair(userId: string) {
+    const data = { _id: userId };
+
+    const refreshToken = await this.jwtService.signAsync(data, {
+      expiresIn: "15d"
+    });
+    const accessToken = await this.jwtService.signAsync(data, {
+      expiresIn: "1h"
+    });
+    return { refreshToken, accessToken };
   }
 
-  returnUserFieslds(user:UserModel){
+  returnUserFields(user: UserModel) {
     return {
       _id: user._id,
       email: user.email,
       isAdmin: user.isAdmin
-    }
+    };
   }
 }
 
